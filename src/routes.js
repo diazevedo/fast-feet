1b77412e2d830a90ab931789b30e60d8eb237cb7
Diego Azevedo <dapptn@hotmail.com>
2020-03-01 02:22:42 +1100

    sucrase and nodemon

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
-const { Router } = require("express");
+import { Router } from "express";
 
 const routes = new Router();
 
 routes.get("/", (req, res) => {
   return res.json({ message: "Working..." });
 });
+
+export default routes;
