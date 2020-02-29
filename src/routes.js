b88964fdf9cbd2bdc4941b76cc02450c564418c8
Diego Azevedo <dapptn@hotmail.com>
2020-03-01 03:11:26 +1100

    eslint babel prettier

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Working..." });
-});
+routes.get('/', (req, res) => res.json({ message: 'Working...' }));
 
 export default routes;
